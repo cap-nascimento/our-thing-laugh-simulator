707e9966e5eafd3a2c5144f5ea0d319d05e8da4d
Anderson Vieira do Nascimento <anascimentovieira2@gmail.com>
2021-01-20 00:46:40 -0300

    improving style

--- simulator/views.py
+++ simulator/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'simulator/index.html')
+
+
+def laughs(request):
+    array = [
+        'Robertinho', 'Canella', 'Laura Seraphim', 'Lucas Inutilismo'
+    ]
+    return render(request, 'simulator/laughs.html', {
+        "nossers": sorted(array),
+        "audios": [x for x in range(10)]
+    })
+
+
+def add_laugh(request):
+    return render(request, 'simulator/addLaugh.html')
